11af64b1d6e25c06260415b56b7a4d6e1f1cd1fc
Van Nguyen <vannguyen093@gmail.com>
2022-10-01 12:18:15 +0700

    Demo random

---
+++ src/javaTester/Topic_04_Random_Number.java
@@ -0,0 +1,12 @@
+package javaTester;
+
+import java.util.Random;
+
+public class Topic_04_Random_Number {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		rand.nextInt(99999);
+	}
+
+}
