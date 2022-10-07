9a554edf4e09eea8fd776585353eece57b4e7497
Van Nguyen <vannguyen093@gmail.com>
2022-10-07 16:37:23 +0700

    Add Topic 16

---
+++ src/webdriver/Topic_16_Frame_iFrame.java
@@ -0,0 +1,56 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_16_Frame_iFrame {
+	WebDriver driver;
+	JavascriptExecutor jsExecutor;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if(osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+		driver = new FirefoxDriver();
+		jsExecutor = (JavascriptExecutor) driver;
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01_() {
+		driver.get("");
+	}
+
+	@Test
+	public void TC_02_() {
+		
+	}
+
+	@Test
+	public void TC_03_() {
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	public void sleepInSecond (long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
